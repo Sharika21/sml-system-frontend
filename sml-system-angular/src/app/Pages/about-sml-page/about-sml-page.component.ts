ba3995b51ed9655146433beaf1d87c105fa91c7d
Sharika Kanti Narsing <skantinarsing@gmail.com>
2024-05-19 23:39:24 +0200

    Fix styling

--- sml-system-angular/src/app/Pages/about-sml-page/about-sml-page.component.ts
+++ sml-system-angular/src/app/Pages/about-sml-page/about-sml-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-about-sml-page',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrl: './about-sml-page.component.scss'
 })
 export class AboutSmlPageComponent {
+  constructor(private router: Router) {}
 
+  navigateToHome() {
+    this.router.navigate(['/home']);
+  }
 }
